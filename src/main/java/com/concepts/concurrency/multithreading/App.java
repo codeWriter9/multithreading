e8f88cd6eb528a572fb426430ffe4cab19f4a9e2
Sanjay <Sanjay.X.Ghosh@gmail.com>
2018-10-21 13:21:49 +0530

    First Commit

---
+++ src/main/java/com/concepts/concurrency/multithreading/App.java
@@ -0,0 +1,13 @@
+package com.concepts.concurrency.multithreading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
